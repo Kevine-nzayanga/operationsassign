298968111cae72f955a3e95af7e41db4aa8798df
Kevine <kevinenzayanga@gmail.com>
2023-02-09 07:44:17 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+fun main() {
+  names(name = "Ada")
+
+   div(12.0,32.0)
+    var result = div(24.0,32.0)
+        println(result)
+
+    add (12, 23, 34, 4)
+    var sum = add(12,23,34,4)
+    println(sum)
+
+    fact(interestingfact = "I have visited the whole of East Africa")
+
+}
+fun names( name: String) {
+var names = ("Hello" +" "+name)
+println(names)
+
+}
+fun div(num1: Double, num2: Double): Double {
+    return  num1%num2
+
+
+}
+fun add(no1:Int, no2:Int, no3: Int, no4:Int): Int {
+var sum = no1 +no2 + no3 +no4
+return sum
+println(sum)
+}
+fun fact(interestingfact: String){
+var fact= "I have visited the whole of East Africa"
+println(fact)
+}
